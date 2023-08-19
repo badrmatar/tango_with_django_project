d9232358e13575f754eb4fe0909632060d45b50d
“Bader <272272m@student.gla.ac.uk>
2023-08-19 11:53:11 +0000

    I added some studd in urls and created a new url file

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include
 from rango import views
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rango/', include('rango.urls'))
     path('', views.index, name='index'),
 
 ]
