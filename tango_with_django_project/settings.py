8bfa27806c7aa4a53b966d011ab7b3ff12d82905
“Bader <272272m@student.gla.ac.uk>
2023-08-21 15:19:42 +0000

    32

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -21,6 +21,7 @@ MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -107,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 STATICFILES_DIRS = [STATIC_DIR, ]
-STATIC_DIRS = [os.path.join(BASE_DIR, 'static'),]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
 
