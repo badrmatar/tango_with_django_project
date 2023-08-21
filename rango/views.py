9fc53edf8898a6a01e4a5c97d663bdbd6df1e792
“Bader <272272m@student.gla.ac.uk>
2023-08-21 14:33:21 +0000

    2

--- rango/views.py
+++ rango/views.py
@@ -7,6 +7,6 @@ def index(request):
     context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
     return render(request, 'rango/index.html', context=context_dict)
 
+
 def about(request):
-    context = {'MEDIA_URL': settings.MEDIA_URL}
-    return render(request, 'rango/about.html', context)
+    return render(request, 'rango/about.html')
