a9f8a6d5b25dbf59ea50f02a47c47f887cfe8b64
“Bader <272272m@student.gla.ac.uk>
2023-08-24 16:46:36 +0000

    quicl fix

--- rango/models.py
+++ rango/models.py
@@ -7,7 +7,7 @@ class Category(models.Model):
 
     name = models.CharField(max_length=128, unique=True)
     likes = models.IntegerField(default=0)
-    view = models.IntegerField(default=0)
+    views = models.IntegerField(default=0)
     def __str__(self):
         return self.name
 
